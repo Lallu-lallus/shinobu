18a2919eea789d17a8a46580b07434a83729f59c
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-02 18:01:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -44,7 +44,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https//t.me/Shinobufl_Bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
